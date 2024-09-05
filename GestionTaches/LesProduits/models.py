e01965c40bdc33bb764d97fc45ca66f42e29508a
Mechain Romain <o22202608@or-iut-i102-f14.iut45.univ-orleans.fr>
2024-09-05 09:32:59 +0200

    fin 2eme cour

--- GestionTaches/LesProduits/models.py
+++ GestionTaches/LesProduits/models.py
@@ -4,19 +4,30 @@ from django.db import models
 
 from django.db import models
 
+class Statut(models.Model) : 
+    numStatut = models.IntegerField()
+    libele = models.CharField(max_length=250)
+
+    def __unicode__(self) :
+        return "numStatut : {0} libele : {1}".format(self.numStatut, self.libele)
+
 class Product(models.Model) : 
     name = models.CharField(max_length=250)
     code = models.IntegerField()
+    prixHT = models.FloatField()
+    dateFabrication = models.DateField(auto_now_add=True)
+    statut = models.ForeignKey("Statut",on_delete=models.CASCADE)
 
     def __unicode__(self) :
-        return "{0} [{1}]".format(self.name, self.code)
+        return "name : {0} code : {1} prixHT : {2}euros dateFabrication {3} statut : {4}".format(self.name, self.code, self.prixHT, self.dateFabrication, self.statut.libele)
     
 class ProductItem(models.Model) : 
+    codeItem = models.IntegerField()
     color = models.CharField(max_length=100)
     product = models.ForeignKey("Product",on_delete=models.CASCADE)
 
     def __unicode__(self) :
-        return "{0} [{1}] [{2}]".format(self.product.name, self.product.code, self.color)
+        return "codeItem : {0} color: {1} product name : {2}".format(self.codeItem, self.color, self.product.name)
 
 
 #python3 manage.py makemigrations LesProduits
