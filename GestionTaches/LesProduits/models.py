90288b5b903696679b7f6e70da68344d8c44917d
Mechain Romain <o22202608@or-iut-i102-f14.iut45.univ-orleans.fr>
2024-09-05 09:05:37 +0200

    Mise en place de classes dans le model

--- GestionTaches/LesProduits/models.py
+++ GestionTaches/LesProduits/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Product(models.Model) : 
+    name = models.CharField(max_length=250)
+    code = models.IntegerField()
+
+    def __unicode__(self) :
+        return "{0} [{1}]".format(self.name, self.code)
+    
+class ProductItem(models.Model) : 
+    color = models.CharField(max_length=100)
+    product = models.ForeignKey("Product",on_delete=models.CASCADE)
+
+    def __unicode__(self) :
+        return "{0} [{1}] [{2}]".format(self.product.name, self.product.code, self.color)
+
+
+#python3 manage.py makemigrations LesProduits
+#python3 manage.py sqlmigrate LesProduits 0001
+#python manage.py migrate
